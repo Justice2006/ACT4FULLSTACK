089a2f2a47db544e18b4ba85c3ed94ea1c5a48ef
Lucio Ashcroft <70651223+Justice2006@users.noreply.github.com>
2025-02-15 13:06:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`⚡ Servidor corriendo en http://localhost:${PORT}`);
 });
